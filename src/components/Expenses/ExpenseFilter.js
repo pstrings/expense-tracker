dd18da097bac8559d4624e8bd033d6a72a6c2304
Suyash <paradisestrings99@gmail.com>
2022-05-24 15:40:14 +0530

    Add Expense Filter

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,15 +1,9 @@
-import {useState} from 'react';
-
 import './ExpenseFilter.css';
 
 const ExpensesFilter = (props) => {
 
-    const [selectedDate, setSelectedDate] = useState("2022")
-
-    const setSelectedHandler = event => {
-        setSelectedDate(event.target.value)
-
-        console.log(selectedDate)
+    const setSelectedYearHandler = event => {
+      props.onselectFilter(event.target.value)
     }
 
 
@@ -17,7 +11,7 @@ const ExpensesFilter = (props) => {
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select value={selectedDate} onChange={setSelectedHandler}>
+        <select value = {props.selectYear} onChange={setSelectedYearHandler}>
           <option value='2025'>2025</option>
           <option value='2024'>2024</option>
           <option value='2023'>2023</option>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 import ExpenseItems from "./ExpenseItems";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
+import React from "react";
 
 const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2022");
+
+  const selectFilter = (year) => {
+    setSelectedYear(year);
+  };
+
   return (
     <div>
-      <ExpenseFilter />
       <Card className="expenses">
+        <ExpenseFilter selectYear = {selectedYear} onselectFilter = {selectFilter} />
         <ExpenseItems
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
